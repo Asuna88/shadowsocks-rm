2849c55fa5858c3dd1b184f0f3a41c9cb50bd2df
ting <ting.wu@hqtec.cn>
2016-05-05 13:28:20 +0800

    signed

--- shadowsocks/config.py
+++ shadowsocks/config.py
@@ -17,8 +17,9 @@ MANAGE_PORT = 23333
 #if you want bind ipv4 and ipv6 '[::]'
 #if you want bind all of ipv4 if '0.0.0.0'
 #if you want bind all of if only '4.4.4.4'
-SS_BIND_IP = '0.0.0.0'
-SS_METHOD = 'rc4-md5'
+SS_BIND_IP = '127.0.0.1'
+SS_METHOD = 'chacha20'
+SS_NODE = '2'
 
 SS_FAST_OPEN = False
 #LOG CONFIG
